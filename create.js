78b20e8bc4c2c56accde34ebdd22628902276910
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2023-11-03 16:25:02 +0600

    Add files via upload

--- create.js
+++ create.js
@@ -254,6 +254,12 @@ async function createAccount() {
                         }
                     } else {
                         console.log('|*|-TIMEOUT:4-')
+                        let url = await page.url()
+                        await patchAxios(BASE_URL+'error/4.json', JSON.stringify({ data:url }), {
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                            }
+                        })
                         await errorHandling()
                     }
                 } else {
